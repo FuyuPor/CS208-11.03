787a39e420965cba85c727b14de3c86811d667db
Fuyu Y Por <fuyupor@u.boisestate.edu>
2025-11-08 21:25:34 +0000

    Complete Task 5

--- dist/script.js
+++ dist/script.js
@@ -26,10 +26,17 @@ function addTask(NEWTASK){
     let list = document.createElement("li");
     list.innerHTML = "<span class='task-text'>" + NEWTASK  + "</span><button class='done-btn'>&#10006;</button>";
     document.querySelector("ol").appendChild(list);
+    let doneButtons = document.querySelectorAll(".done-btn");
+    doneButtons[doneButtons.length-1].addEventListener("click", removeTask);
 }
 
 function keyupHandler(event){
     if (event.key == "Enter") {
         addBtnClick();
     }
+}
+
+function removeTask(event){
+    let Xbutton = event.target.parentNode;
+    Xbutton.parentNode.removeChild(Xbutton);
 }
