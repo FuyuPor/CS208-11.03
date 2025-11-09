5ee1b6fef109a15232320dac3a9ad4064056977e
Fuyu Y Por <fuyupor@u.boisestate.edu>
2025-11-09 07:25:35 +0000

    Completed Lab 11

--- dist/script.js
+++ dist/script.js
@@ -1,24 +1,19 @@
 const addButton = document.getElementById("addButton");
 const textBox = document.getElementById("addTask");
 
+// Adds a new task from the textbox into the list
 addButton.addEventListener("click", function(event) {
     event.preventDefault(); 
     addBtnClick();
 });
-
-window.addEventListener("DOMContentLoaded", domLoaded);
 
 function addBtnClick(){
     let task = document.getElementById("addTask").value;
     if (task != ""){
         addTask(task);
         document.getElementById("addTask").value = "";
-        textBox.focus()
+        textBox.focus();
     }
-}
-
-function domLoaded(){
-    textBox.addEventListener("keyup", keyupHandler);
 }
 
 function addTask(NEWTASK){
@@ -29,12 +24,20 @@ function addTask(NEWTASK){
     doneButtons[doneButtons.length-1].addEventListener("click", removeTask);
 }
 
+// Makes the Enter Key function the same as the Add Button
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+function domLoaded(){
+    textBox.addEventListener("keyup", keyupHandler);
+}
+
 function keyupHandler(event){
     if (event.key == "Enter") {
         addBtnClick();
     }
 }
 
+// Removes a task when the X is pressed
 function removeTask(event){
     let Xbutton = event.target.parentNode;
     Xbutton.parentNode.removeChild(Xbutton);
